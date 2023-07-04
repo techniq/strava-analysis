3e250d05df8dd54e5e5d8177393136cdc5b6f045
Sean Lynch <techniq35@gmail.com>
2023-07-03 23:06:21 -0400

    Change activities from +page.server.ts and use cache headers (5 min) to reduce API calls

--- src/routes/activities/+page.server.ts
+++ src/routes/activities/+page.server.ts
@@ -1,13 +1,17 @@
 import { Strava } from '$lib/api.js';
 
-export async function load({ parent, url }) {
+export async function load({ parent, url, setHeaders }) {
   const { accessToken, athlete } = await parent();
 
-  const athleteId = Number(url.searchParams.get('athlete')) || athlete.id;
+  const athleteId = Number(url.searchParams.get('athlete')) || athlete;
   const per_page = url.searchParams.get('per_page') ?? 25;
   const page = url.searchParams.get('page') ?? 1;
 
   const strava = new Strava(accessToken);
+
+  setHeaders({
+    'cache-control': `max-age=${60 * 5}` // 5 min
+  });
 
   return {
     activities: strava.api('/athlete/activities', { data: { per_page, page } }),
