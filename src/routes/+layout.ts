e279f4b23dfe95309fae590ecd8de78763168c99
Sean Lynch <techniq35@gmail.com>
2023-07-06 09:41:51 -0400

    Redirect to login if access_token expired

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,16 +1,23 @@
 import { Strava } from '$lib/api';
+import { redirect } from '@sveltejs/kit';
 
 export const ssr = false;
 
 export async function load({ data }) {
+  let athlete: any = null;
+  if (data.accessToken) {
+    const strava = new Strava(data.accessToken);
+    athlete = await strava.api('/athlete');
+    if (athlete.errors) {
+      const [error] = athlete.errors;
+      if (error.field === 'access_token' && error.code === 'invalid') {
+        throw redirect(302, '/auth/login');
+      }
+    }
+  }
+
   return {
     ...data,
-    athlete: data.accessToken ? fetchAthlete(data.accessToken) : null
+    athlete
   };
-}
-
-async function fetchAthlete(accessToken: string) {
-  const strava = new Strava(accessToken);
-  const athlete = await strava.api('/athlete');
-  return athlete;
 }
