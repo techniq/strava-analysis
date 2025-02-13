45dca3d63c9569f8dd5a846901787847ccaa362c
Sean Lynch <techniq35@gmail.com>
2025-02-12 23:25:07 -0500

    Migrade overview chart to simplified BarChart

--- src/routes/analysis/overview/+page.svelte
+++ src/routes/analysis/overview/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-  import { scaleBand } from 'd3-scale';
   import { timeMonth, timeYear } from 'd3-time';
   import { format as dateFormat } from 'date-fns';
 
   import { Card } from 'svelte-ux';
-  import { Axis, Bars, Chart, Highlight, Svg, Tooltip } from 'layerchart';
-  import { format, formatDate, PeriodType } from '@layerstack/utils';
+  import { BarChart, Tooltip } from 'layerchart';
+  import { format, PeriodType } from '@layerstack/utils';
   import { promiseStore } from '@layerstack/svelte-stores';
 
   import { metersToMiles } from '$lib/utils.js';
@@ -29,47 +28,57 @@
       )} total miles"
       class="h-[300px]"
     >
-      <Chart
+      <BarChart
         data={data.values}
         x={(d) => timeMonth(d.start_date)}
-        xScale={scaleBand().padding(0.1)}
+        bandPadding={0.1}
         xDomain={timeMonth.range(...startDateExtent)}
         y={(d) => metersToMiles(d.distance)}
-        yDomain={[0, null]}
-        yNice
         padding={{ left: 32, bottom: 24, right: 16 }}
         tooltip={{ mode: 'voronoi' }}
+        props={{
+          xAxis: {
+            grid: true,
+            ticks: timeYear.range(...startDateExtent),
+            format: PeriodType.CalendarYear
+          },
+          yAxis: {
+            format: 'metric'
+          },
+          grid: {
+            y: { style: 'stroke-dasharray: 2' }
+          },
+          bars: {
+            rounded: false,
+            class: 'fill-blue-500/10 stroke-none'
+          },
+          highlight: {
+            area: false,
+            lines: true,
+            points: { class: 'fill-blue-500' }
+          }
+        }}
       >
-        <Svg>
-          <Axis placement="left" grid={{ style: 'stroke-dasharray: 2' }} rule format="metric" />
-          <Axis
-            placement="bottom"
-            grid
-            rule
-            ticks={timeYear.range(...startDateExtent)}
-            format={(d) => formatDate(d, PeriodType.CalendarYear)}
-          />
-          <Bars class="fill-blue-500/10" />
-          <Highlight points={{ class: 'fill-blue-500' }} lines />
-        </Svg>
-        <Tooltip.Root x="data" y="data" let:data>
-          <Tooltip.Header>{dateFormat(data.start_date, 'eee, MMMM do')}</Tooltip.Header>
-          <Tooltip.List>
-            <Tooltip.Item
-              label="Distance"
-              value={metersToMiles(data.distance)}
-              format="decimal"
-              valueAlign="right"
-            />
-            <Tooltip.Item
-              label="Total Distance"
-              value={metersToMiles(data.totalDistance)}
-              format="decimal"
-              valueAlign="right"
-            />
-          </Tooltip.List>
-        </Tooltip.Root>
-      </Chart>
+        <svelte:fragment slot="tooltip">
+          <Tooltip.Root x="data" y="data" let:data>
+            <Tooltip.Header>{dateFormat(data.start_date, 'eee, MMMM do')}</Tooltip.Header>
+            <Tooltip.List>
+              <Tooltip.Item
+                label="Distance"
+                value={metersToMiles(data.distance)}
+                format="decimal"
+                valueAlign="right"
+              />
+              <Tooltip.Item
+                label="Total Distance"
+                value={metersToMiles(data.totalDistance)}
+                format="decimal"
+                valueAlign="right"
+              />
+            </Tooltip.List>
+          </Tooltip.Root>
+        </svelte:fragment>
+      </BarChart>
     </Card>
   {/each}
 </div>
